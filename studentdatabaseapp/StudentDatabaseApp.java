84d7068b5f2ccfd6e8536785c8f840c7ff72a57d
Nathan Y. C <50110608+TheWitness4@users.noreply.github.com>
2019-07-11 12:39:12 -0400

    Add files via upload

---
+++ studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,14 @@
+package studentdatabaseapp;
+
+public class StudentDatabaseApp {
+
+	public static void main(String[] args) {
+		Student stu1 = new Student();
+		stu1.enroll();
+		stu1.payTuition();
+		// Ask how many new users to add
+		
+		// Create n number of new students
+	}
+
+}
